2410416cce9fd445ace1c5a19ef363fd3d00f2e1
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-17 07:48:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 11
+
+  loop do
+    number -= 1
+    puts "#{number} SECOND(S)!"
+    if number <= 1
+      break
+    end
+  end
+
+"HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(pause)
+  sleep 7
 end
