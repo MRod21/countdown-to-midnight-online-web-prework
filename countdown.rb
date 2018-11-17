aa205cc4a3f993e13328d5684106489ecc09a836
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-17 07:34:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 11
+
+  loop do
+    number -= 1
+    puts "#{number} SECOND(S)!"
+    if number = 0
+      break
+  end
+end
+
 end
